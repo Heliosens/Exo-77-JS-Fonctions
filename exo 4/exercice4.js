cc60b008e8813cb382a22ba75c4050ae08c3bf9d
Sylvie-B <unsolo@hotmail.fr>
2021-11-18 16:49:36 +0100

    creation de fonction ajout de texte

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,10 +1,11 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
-
+function addText(){
+    let target = document.getElementById("container");
+    target.innerHTML = "Voilà le texte";
+}
 
 /* Appeler la fonction */
 
-
-
-
+addText();
 
